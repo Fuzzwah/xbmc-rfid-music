e55ae4cb84c685c07267e8208b171c6123f38639
Fuzzwah <rob.crouch@gmail.com>
2014-07-09 13:29:05 -0700

    addcard.py now waits for swipe

--- addcard.py
+++ addcard.py
@@ -1,11 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import sys, shelve
+from conf import *
+from evdev import InputDevice, categorize, ecodes
+
+# this is the rfid reader
+dev = InputDevice(rfidreader)
+
+# grab the rfid reader device
+dev.grab()
+
+cardnumber = []
 
 try:
-   outdb = shelve.open( "playlists.db" )
+   db = shelve.open("playlists.db", writeback=True)
 except IOError:
    print >> sys.stderr, "File could not be opened"
    sys.exit( 1 )
 
-outdb['0006794466'] = 'playlist.m3u'
+# wait for events from the rfid reader
+for event in dev.read_loop():
+	print 'Swipe the card you wish to assign to playlist: {pl}'.format(pl=sys.argv[1])
+	# if we get a "key pressed down" event
+	if event.type == ecodes.EV_KEY and event.value == 1:
+		# if it is the enter key we've hit the end of a card number
+		if event.code == 28:
+			# merge the list in to a string
+			card = ''.join(str(num) for num in cardnumber)
+			# add (or update) the entry 
+			db[card] = sys.argv[1]
+			# we're done so break out
+			break
+		else:
+			# the event code is 1 more than the actual number key
+			number = event.code - 1
+			# except for event id 10, which is actually KP_0
+			if number == 10:
+				number = 0
+			# stick the number onto the end of our list
+			cardnumber.append(number)
 
-outdb.close()   # close shelve file
+db.close()   # close shelve file
